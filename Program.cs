c0e779048e079fa2039b03ce4eb0695cc1ae74d1
jiakmj <jiakmj66@gmail.com>
2025-02-03 14:24:46 +0900

    별찍기

--- Program.cs
+++ Program.cs
@@ -1,11 +1,29 @@
-﻿namespace Test20250203
+﻿using System;
+
+namespace Test20250203
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!01");
+            int[,] data = new int[5, 5];
+            int size = 5;
+
+            for(int j = 0; j <= size; j++)
+                {
+
+                for (int i = 0; i < size - j; i++)
+                {
+                    Console.Write(' ');
+                }
+
+                for (int i = 0; i < j; i++)
+                {
+                    Console.Write('*');                   
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
