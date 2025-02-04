0ea8694c428240b10861d4de92c63a6fe4f90cba
jiakmj <jiakmj66@gmail.com>
2025-02-04 15:00:33 +0900

    블랙잭 3장 받고 컴퓨터와 승부

--- T20250204/Program.cs
+++ T20250204/Program.cs
@@ -39,16 +39,78 @@ namespace T20250204
             Spade = 3,
             Max,
         }
+        
+        static int GetScore(int cardNumber)
+        {
+            int value = (((cardNumber - 1) % 13) + 1);
+            return value > 10 ? 10 : value;
+        }
+
+        static void PrintCardList(int[] deck)
+        {
+            //computer
+            Console.WriteLine("Computer");
+            for (int i = 0; i < 3; ++i)
+            {
+                Console.WriteLine($"{CheckCardType(deck[i]).ToString()}{CheckCardName(deck[i])}");
+            }
+            Console.WriteLine("---------");
+            //player
+            Console.WriteLine("Player");
+            for (int i = 3; i < 6; ++i)
+            {
+                Console.WriteLine($"{CheckCardType(deck[i]).ToString()}{CheckCardName(deck[i])}");
+            }
+            Console.WriteLine("---------");
+        }
+
 
         static void Print(int[] deck)
         {
-            for (int i = 0; i < 8; ++i)
+            PrintCardList(deck);
+
+            //카드 3장 받고 합하기
+            int computerScore = GetScore(deck[0]) + GetScore(deck[1]) + GetScore(deck[2]);
+            int playerScore = GetScore(deck[3]) + GetScore(deck[4]) + GetScore(deck[5]);
+
+            Console.WriteLine($"Computer score: {computerScore}, Player score: {playerScore}");
+
+            if (playerScore >= 21 && computerScore < 21)
             {
-                Console.WriteLine(CheckCardType(deck[i]).ToString() + " " + CheckCardName(deck[i]));
+                //computer win
+                Console.WriteLine("Computer Win");
+
+            }
+            else if (playerScore <= 21 && computerScore >= 21)
+            {
+                //player win
+                Console.WriteLine("Player Win");
+            }
+            else if (playerScore >= 21 && computerScore >= 21)
+            {
+                //player win
+                Console.WriteLine("Player Win");
+            }
+            else if (computerScore <= playerScore)
+            {
+                //player win
+                Console.WriteLine("Player Win");
+            }
+            else // (computerScore > playerScore)
+            {
+                //computer win
+                Console.WriteLine("Computer Win");
             }
 
-        }
+            //for (int i = 0; i < 4; ++i)
+            //{
+            //    Console.WriteLine($"{CheckCardType(deck[i]).ToString()}{CheckCardName(deck[i])}");
+            //}
 
+        }     
+        
+
+        //value 1 - 13
         static CardType CheckCardType(int CardNumber)
         {
             int valueType = (CardNumber - 1) / 13;
@@ -113,18 +175,7 @@ namespace T20250204
             Shuffle(deck);
 
             Print(deck);
-            // 1 - 13 -> Heart, 1 -> A, 11 -> J, 12 -> Q, 13 -> K
-            // 14 - 26 -> Diamond
-            // 27 - 39 -> Clover
-            // 40 - 52 -> Spade           
-
-
-
-        }
-       
-
-
-
+           
+        }      
     }
-
 }
